b5e4e139d13dae2865a9176c9c4556f0edf1a1b2
Saif ur Rehman <90093187+saifirehman@users.noreply.github.com>
2024-08-16 23:13:37 +0500

    Add files via upload

---
+++ Credit.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Credit() {
+  return (
+    <div class="credit">
+      <p>
+        Coded by
+        <a href="https://github.com/saifirehman"> SAIF UR REHMAN</a>,{" "}
+        <a href="https://github.com/saifirehman">Open sourced on Github.</a>{" "}
+      </p>
+    </div>
+  );
+}
